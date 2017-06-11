d3f80df2a718d47fac3ee8f729d1b313d5becc22
EnterpriseCV <502101600@qq.com>
2017-06-11 20:04:56 +0800

    deletewrongfile

--- src/main/java/controller/UsersController.java
+++ src/main/java/controller/UsersController.java
@@ -1,9 +1,8 @@
 package controller;
 
-import bean.Comment_Maoyan;
+import bean.Maoyan.Comment_Maoyan;
+import bean.Maoyan.Movie_Maoyan;
 import bean.Movie;
-import bean.Movie_Maoyan;
-import bean.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
