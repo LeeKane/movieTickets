30df1eee58150e9cf713751ea7b7733f080de1d4
Leekane <lk1403994707@outlook.com>
2017-06-11 18:33:06 +0800

    comment

--- src/main/java/controller/UsersController.java
+++ src/main/java/controller/UsersController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import bean.Comment_Maoyan;
 import bean.Movie;
+import bean.Movie_Maoyan;
 import bean.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 import service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
+import service.MovieService_Maoyan;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +23,8 @@ import java.util.Map;
 public class UsersController {
     @Autowired
     private MovieService movieService;
+    @Autowired
+    private MovieService_Maoyan movieService_maoyan;
 
     @RequestMapping(value = "/showUser",method = RequestMethod.GET)
     public String showUser(@RequestParam("id") String id, ModelMap modelMap) {
@@ -41,7 +46,7 @@ public class UsersController {
     @ResponseBody
     @RequestMapping(value = "/getAllMovies",method = RequestMethod.GET)
     public Map getAllMovies(){
-        List<Movie> movies=movieService.getAllMovies();
+        List<Movie_Maoyan> movies=movieService_maoyan.getAllMovie();
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("movies",movies);
         return result;
@@ -49,10 +54,12 @@ public class UsersController {
     @RequestMapping(value = "/movieInfo")
     public ModelAndView movieInfo(HttpServletRequest request,
                                   HttpServletResponse response) throws Exception {
-        int id = Integer.parseInt((String) request.getParameter("id"));
-        Movie movie=movieService.getMovieById(id);
+        String id = (String) request.getParameter("id");
+        Movie_Maoyan movie=movieService_maoyan.getMovieById(id);
+        List<Comment_Maoyan> comments = movieService_maoyan.getCommentByMovieId(id);
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("movie",movie);
+        result.put("comments",comments);
         return new ModelAndView("movieInfo", "result", result);
     }
 
---
+++ src/main/webapp/static/js/comment.js
@@ -0,0 +1,3 @@
+/**
+ * Created by LeeKane on 2017/6/11.
+ */
